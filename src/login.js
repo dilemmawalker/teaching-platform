c1b2bdb96d478a2727b801ba9c811b2d0be433ae
yash handa <yashhanda500@gmail.com>
2021-04-06 23:59:06 +0530

    update

---
+++ src/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+function login(){
+    return(
+        <div>
+            <p>login page.</p>
+        </div>
+    );
+}
+export default login;
